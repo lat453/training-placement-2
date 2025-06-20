2d854781045889ffa5aa856e203580813338c302
lat453 <151341442+lat453@users.noreply.github.com>
2025-06-20 20:02:49 +0530

    Create Staircase.java

---
+++ 20-06-25/Staircase.java
@@ -0,0 +1,7 @@
+void staircase(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < n - i; j++) System.out.print(" ");
+        for (int j = 0; j < i; j++) System.out.print("#");
+        System.out.println();
+    }
+}
