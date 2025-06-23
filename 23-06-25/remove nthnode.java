03e3cbdaec9d1db0416fe5fce25c30fd546dcc54
lat453 <151341442+lat453@users.noreply.github.com>
2025-06-23 19:57:07 +0530

    Create remove nthnode.java

---
+++ 23-06-25/remove nthnode.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode first = dummy;
+        ListNode second = dummy;
+        for (int i = 0; i <= n; i++) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+}
