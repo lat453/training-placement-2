cc08ec10956ac38c72a13ca743e09f59c1b65393
lat453 <151341442+lat453@users.noreply.github.com>
2025-06-24 21:48:49 +0530

    Create vardatatypes.java

---
+++ 24-06-25/vardatatypes.java
@@ -0,0 +1,15 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+        int age = 20;
+        double price = 99.99;
+        char grade = 'A';
+        boolean isJavaFun = true;
+        String name = "Latchana";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java Fun: " + isJavaFun);
+    }
+}
